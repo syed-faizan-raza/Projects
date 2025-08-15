a4e63d0a6b91a273f8fdde029e0cbacc251a07a0
syed-faizan-raza <162629419+syed-faizan-raza@users.noreply.github.com>
2025-08-15 13:24:44 +0400

    Delete Java based text game usign desgin patterns and phone sensors directory

--- Java based text game usign desgin patterns and phone sensors/scripts/GoldenPotion.java
+++
@@ -1,5 +0,0 @@
-package battlenet;
-
-public class GoldenPotion extends Potion {
-    @Override protected void addIngredients(){ UI.getInstance().UIWrite("Adding Golden powder"); }
-}
--- Java based text game usign desgin patterns and phone sensors/scripts/Subject.java
+++
@@ -1,7 +0,0 @@
-package battlenet;
-
-public interface Subject {
-    void publishMessage(Message m);
-    void addObserver(Observer o);
-    void removeObserver(Observer o);
-}
